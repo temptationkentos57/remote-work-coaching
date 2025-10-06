fff3034c5133cdc0f72f04cc3ce28b300111151b
temptationkentos57 <kreetlannoo@hotmail.com>
2025-10-06 20:08:14 +0200

    Fix greeting message to English for consistency.

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Chào mừng đến với Remote Work Coaching!');
+  res.send('Welcome to Remote Work Coaching!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server đang chạy trên http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
